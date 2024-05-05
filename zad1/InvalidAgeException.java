41ce9aaf460031da4a2360ad8cf3ef02fba47e32
zarzyja <162029745+zarzyja@users.noreply.github.com>
2024-05-05 13:54:21 +0200

    1

---
+++ zad1/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package zad1;
+
+public class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
---
+++ zad1/Person.java
@@ -0,0 +1,34 @@
+package zad1;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) throws InvalidAgeException {
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeException("Wiek musi mieścić się w zakresie od 0 do 125");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) throws InvalidAgeException {
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeException("Wiek musi mieścić się w zakresie od 0 do 125");
+        }
+        this.age = age;
+    }
+}
