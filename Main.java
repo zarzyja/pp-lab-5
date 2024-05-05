2ab61930c5c96f3e976c0d75ae2b7791642b86b5
zarzyja <162029745+zarzyja@users.noreply.github.com>
2024-05-05 14:15:50 +0200

    1

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Jan Kowalski", 30);
+            System.out.println("Imię: " + person.getName());
+            System.out.println("Wiek: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.err.println("Błąd: " + e.getMessage());
+        }
+    }
+}
---
+++ zad1/company.exceptions/zad/company/exceptions/company/exceptions/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package company.exceptions;
+
+public class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
