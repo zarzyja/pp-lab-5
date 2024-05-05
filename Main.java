c832440b216cdaba3c0a6ab610bd8ff686f43030
zarzyja <162029745+zarzyja@users.noreply.github.com>
2024-05-05 15:07:31 +0200

    5

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int sum = MathUtils.add(num1, num2);
-        System.out.println("Suma: " + sum);
+        Person[] people = new Person[5];
+        int b = 10;
 
-        // Użyj sumy w metodzie sendMessage (zakładając, że metoda sendMessage istnieje)
-        sendMessage("Wynik dodawania: " + sum);
+        try {
+            for (int i = 0; i < people.length; i++) {
+                people[i] = new Person("Imię " + (i + 1), 30 + i);
+            }
+
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+                sendMessage(person.getName() + ", lat " + person.getAge() + ", wynik: " + result);
+            }
+        } catch (InvalidAgeException e) {
+            System.err.println("Błąd: " + e.getMessage());
+        }
     }
 }
